2f68026dd1a37aaa78bfbcaff4e2f68239f2f228
inmkaklij <a@b>
2022-12-23 17:27:25 +0530

    Code changes for run scan backend integration
    [git-p4: depot-paths = "//depot/ws/products/securitypolicymanager/2.0_cyberintel/": change = 512849]

--- src/java/com/marimba/apps/subscriptionmanager/webapp/util/RunScanHandler.java
+++ src/java/com/marimba/apps/subscriptionmanager/webapp/util/RunScanHandler.java
@@ -47,14 +47,19 @@ public class RunScanHandler {
 	 */
 	public String processRunScanCLI(RunScanForm formbean) {
 
-		String responseMsg = "runscan action CLI execute method has been called ";
+		String responseMsg = "Runscan action CLI execute method has been called ";
 
 		System.out.println(" Form bean for CLI is received :" + formbean);
-		List<String> endDeviceList = Arrays.asList(formbean.getEndDevicesArr());
-		if (endDeviceList.size() > 0) {
-			scanSelectedEndDevices(endDeviceList);
+		if (null != formbean && null != formbean.getEndDevicesArr() && !formbean.getEndDevicesArr().isEmpty()) {
+			String[] ipPortArr = formbean.getEndDevicesArr().split(",");
+			List<String> endDeviceList = Arrays.asList(ipPortArr);
+			if (endDeviceList.size() > 0) {
+				scanSelectedEndDevices(endDeviceList);
+			} else {
+				responseMsg = "No end devices are selected for scan";
+			}
 		} else {
-			responseMsg = "No end devices are selected for scan";
+			System.out.println(" Run scan value is not received in form bean");
 		}
 		return responseMsg;
 	}
@@ -73,34 +78,39 @@ public class RunScanHandler {
 		try {
 			String marimbaSetupPath = System.getenv("BCAC_HOME");
 			String driveName = marimbaSetupPath.split(":")[0];
-			System.out.println(" map txt file path : " + marimbaSetupPath + " Drive : " + driveName);
+			System.out.println(" map txt file path : " + marimbaSetupPath + " \n Drive ID : " + driveName);
 
 			// get tunerAdmin URL from map.txt file located in marimba setup.
 
 			String tunerAdminURL = getTunerAdminURL(marimbaSetupPath);
 
-			for (String ipPort : ipPortList) {
+			if (null != tunerAdminURL && !tunerAdminURL.isEmpty()) {
+				for (String ipPort : ipPortList) {
 
-				String runScanCommand = "runchannel " + tunerAdminURL + " -start -tuner " + ipPort
-						+ " -channel http://azuretx.marimbacastanet.com:5282/vInspector";
+					System.out.println("Received IP:Port is ==> " + ipPort);
+					String runScanCommand = "runchannel " + tunerAdminURL + " -start -tuner " + ipPort
+							+ " -channel http://defensight.marimbacastanet.com:5282/vInspector";
 
-				System.out.println(" Command is ready :" + runScanCommand);
+					System.out.println(" Command is ready to trigger scan:" + runScanCommand);
 
-				ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/" + driveName,
-						"cd \"" + "" + marimbaSetupPath + "\\Tuner\\\"" + "" + " && " + runScanCommand);
+					ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/" + driveName,
+							"cd \"" + "" + marimbaSetupPath + "\\Tuner\\\"" + "" + " && " + runScanCommand);
 
-				builder.redirectErrorStream(true);
-				Process p = builder.start();
-				BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-				String line;
-				while (true) {
-					line = r.readLine();
-					if (line == null) {
-						break;
+					builder.redirectErrorStream(true);
+					Process p = builder.start();
+					BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+					String line;
+					while (true) {
+						line = r.readLine();
+						if (line == null) {
+							break;
+						}
+						System.out.println(" Command execution result :: " + line);
 					}
-					System.out.println(line);
+					System.out.println(" Scan is started for host :" + ipPort);
 				}
-				System.out.println(" Scan is started for host :" + ipPort);
+			} else {
+				System.out.println(" Tunner admin command line channel is not subscribed");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
